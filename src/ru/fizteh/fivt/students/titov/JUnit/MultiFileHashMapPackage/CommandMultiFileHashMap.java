5178ec86b52ade9837edf3de57d9575c164f7145
IgorTitov396 <1hp_phoenix@mail.ru>
2014-12-10 05:25:59 +0300

    V 1.0

--- src/ru/fizteh/fivt/students/titov/JUnit/MultiFileHashMapPackage/CommandMultiFileHashMap.java
+++
@@ -1,6 +0,0 @@
-package ru.fizteh.fivt.students.titov.JUnit.MultiFileHashMapPackage;
-
-import ru.fizteh.fivt.students.titov.JUnit.FileMapPackage.Command;
-
-public abstract class CommandMultiFileHashMap extends Command<MFileHashMap> {
-}
---
+++ src/ru/fizteh/fivt/students/titov/shell/FileUtils.java
@@ -0,0 +1,23 @@
+package ru.fizteh.fivt.students.titov.shell;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class FileUtils {
+    public static boolean mkdir(Path newDirectory) {
+        CommandMkdir command = new CommandMkdir();
+        String[] args = {"mkdir",
+                         newDirectory.toString()
+        };
+        return command.run(args);
+    }
+
+    public static void rmdir(Path newDirectory) throws IOException {
+        if (!Files.isDirectory(newDirectory) | !Files.exists(newDirectory)) {
+            throw new IllegalArgumentException();
+        }
+        FileVisitorDelete myFileVisitorDelete = new FileVisitorDelete();
+        Files.walkFileTree(newDirectory, myFileVisitorDelete);
+    }
+}
