89d3eac3677c618db45c8cd4d801ab352e7b9fb0
IgorTitov396 <1hp_phoenix@mail.ru>
2014-12-25 04:37:40 +0300

    Parallel V1

---
+++ src/ru/fizteh/fivt/students/titov/parallel/multi_file_hash_map/ShowTablesCommand.java
@@ -0,0 +1,25 @@
+package ru.fizteh.fivt.students.titov.parallel.multi_file_hash_map;
+
+import ru.fizteh.fivt.students.titov.parallel.file_map.FileMap;
+
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class ShowTablesCommand extends MultiFileHashMapCommand {
+    public ShowTablesCommand() {
+        initialize("show", 2);
+    }
+
+    @Override
+    public boolean run(MFileHashMap myMap, String[] args) {
+        if (!args[1].equals("tables")) {
+            System.err.println(name + ": wrong arguments");
+            return false;
+        }
+        Set<Entry<String, FileMap>> pairSet = myMap.getTables().entrySet();
+        for (Entry<String, FileMap> oneTable: pairSet) {
+            System.out.println(oneTable.getKey() + " " + oneTable.getValue().size());
+        }
+        return true;
+    }
+}
