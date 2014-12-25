2fe3d3c7c5c775faffca7ed9e1b9d4963645d764
IgorTitov396 <1hp_phoenix@mail.ru>
2014-12-25 04:41:15 +0300

    Delete old

--- src/ru/fizteh/fivt/students/titov/JUnit/file_map/FileMapCommand.java
+++
@@ -1,8 +0,0 @@
-package ru.fizteh.fivt.students.titov.JUnit.file_map;
-
-public abstract class FileMapCommand extends Command<FileMap> {
-    public void initialize(String commandName, int n) {
-        name = commandName;
-        numberOfArguments = n;
-    }
-}
--- src/ru/fizteh/fivt/students/titov/JUnit/file_map/ReadFromDiskException.java
+++
@@ -1,7 +0,0 @@
-package ru.fizteh.fivt.students.titov.JUnit.file_map;
-
-public class ReadFromDiskException extends Exception {
-    public ReadFromDiskException() {
-        super("Can't read from disk");
-    }
-}
